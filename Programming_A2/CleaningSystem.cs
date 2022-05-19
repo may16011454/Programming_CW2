0e2e006d3f9a42ebcaddac20070ec3c157e285db
Logan May <MAY16011454@btc.ac.uk>
2022-05-19 12:35:56 +0100

    added cleaning systems

---
+++ Programming_A2/CleaningSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_A2
+{
+    internal class CleaningSystem
+    {
+        public string Name { get; set; }
+
+        public List<IProperties>? Properties { get; set; }
+        public List<IPerson>? People { get; set; }
+        public List<Booking>? BookingInfo { get; set; }
+        public List<Customer>? CustomerInfo { get; set; }
+        public List<Domestic>? DomesticInfo { get; set; }
+        public List<Commercial>? CommercialInfo { get; set; }
+
+        public CleaningSystem(string name)
+        {
+            Name = name;
+            Properties = new List<IProperties>();
+            People = new List<IPerson>();  
+            BookingInfo = new List<Booking>();
+            CustomerInfo = new List<Customer>();
+            DomesticInfo = new List<Domestic>();
+            CommercialInfo = new List<Commercial>();
+        }
+    }
+}
